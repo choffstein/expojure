f6461e3a5cf5d5e8c71e32595748a4326ec4e0a8
Corey M. Hoffstein <corey.m.hoffstein@gmail.com>
2011-03-11 15:58:31 -0500

    Added project.clj and README

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject expojure "1.0.0-SNAPSHOT"
+  :description "Catch and store all exceptions"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
+  :main expojure.core)
