a48790b95c0009985843e40b2eef57486579fc1c
Corey M. Hoffstein <corey.m.hoffstein@gmail.com>
2011-03-12 22:53:51 -0500

    Created local/global try

--- src/expojure/core.clj
+++ src/expojure/core.clj
@@ -2,8 +2,21 @@
   (:use expojure.dispatchers)
   (:gen-class))
 
-; Huge thanks to amalloy and brehaut in #clojure (irc.freenode.net)
-(defmacro try [execute-clause & arglist]
+; Huge thanks to amalloy and brehaut in #clojure (irc.freenode.net) for helping me write
+; these macros
+
+(defmacro local-try [dispatchers execute-clause & arglist]
+  (let [catch-clauses (filter (comp #{'catch} first) arglist)
+	new-catch-clauses (map
+			   #(let [[_ e-class e-name & e-ret ] %
+				  dispatched-methods (map (fn [d] `(~d ~e-name))
+							  dispatchers)]
+			       `(catch ~e-class ~e-name (do ~@dispatched-methods ~@e-ret)))
+			    catch-clauses)
+	finally-clause (filter (comp #{'finally} first) arglist)]
+    `(try ~execute-clause ~@new-catch-clauses ~@finally-clause)))
+
+(defmacro global-try [execute-clause & arglist]
   (let [catch-clauses (filter (comp #{'catch} first) arglist)
 	new-catch-clauses (map
 			   #(let [[_ e-class e-name & e-ret ] %
