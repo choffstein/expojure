07bd8cb4701a7113647f570fbf2ceb2173dd1473
Baishampayan Ghose <b.ghose@gmail.com>
2011-04-04 18:43:16 +0530

    Use the `doto` macro to perform side-effects on a newly created object instead of doing them in let bindings.
    Use the idiomatic `dot-notation` for creating new objects instead of `new`.

--- src/expojure/util.clj
+++ src/expojure/util.clj
@@ -5,16 +5,16 @@
   (:gen-class))
 
 (defn current-time []
-  (let [df (new SimpleDateFormat "yyy-MM-dd'T'HH:mmZ")]
-    (.toString (.format df (new Date)))))
+  (let [df (SimpleDateFormat. "yyy-MM-dd'T'HH:mmZ")]
+    (.toString (.format df (Date.)))))
 
 (defn stack-trace-as-str [e]
-   (let [sw (new java.io.StringWriter)
-	pw (new java.io.PrintWriter sw true)
-	_ (.printStackTrace e pw) ; side-effect puts stack trace in pw
-	_ (.flush pw)
-	_ (.flush sw )]
-     (.toString sw)))
+  (let [sw (java.io.StringWriter.)
+        pw (doto (java.io.PrintWriter. sw true)
+             (#(.printStackTrace e %)) ; side-effect puts stack trace in pw
+             (.flush))
+        _ (.flush sw)]
+    (.toString sw)))
 
 (defn stack-trace-as-vec [e]
   (reduce #(cons (.toString %2) %1) [] (.getStackTrace e)))
