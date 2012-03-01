426962cce14d7a5532858c77e0f6c3509abed85e
Corey M. Hoffstein <corey@newfoundresearch.com>
2012-03-01 15:57:03 -0500

    Added repos to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject expojure "1.0.0-SNAPSHOT"
   :description "Catch and store all exceptions"
   :dependencies [[org.clojure/clojure "1.3.0-beta1"]]
-  :dev-dependencies [[swank-clojure "1.3.0"
-                     :exclusions [org.clojure/clojure
-                                  org.clojure/clojure-contrib]]])
+
+  :plugins [[s3-wagon-private "1.1.1"]]
+
+  :repositories {"nfr-releases" "s3p://newfound-mvn-repo/releases/"
+                 "nfr-snapshots" "s3p://newfound-mvn-repo/snapshots/"})
